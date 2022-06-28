d96166ffa8c11619672b6e5897effcaf807d09ae
WROracer <67609673+WROracer@users.noreply.github.com>
2022-06-28 14:23:34 +0200

    passe Automation an

--- .space.kts
+++ .space.kts
@@ -34,12 +34,22 @@ job("Build, run tests, publish") {
         }
 }
 
-job("Upload Jar To Server"){
-    container("Upload","maven:3-openjdk-8-slim"){
+job("Upload artifact") {
+    // Docker image must contain the curl tool
+    container("alpine/curl") {
         shellScript {
+            // SOURCE_PATH is path to the build artifact
+            // TARGET_PATH is the destination path in the file repository
+            // Note that each run of the build script creates a separate directory (name = build number)
             content = """
-                echo Build and publish artifacts...
-                ls
+                echo Here go your build activities...
+
+                echo Uploading artifacts...
+                SOURCE_PATH=build-logs/log.txt
+                TARGET_PATH=logs/${'$'}JB_SPACE_EXECUTION_NUMBER/log.txt
+                REPO_URL=https://files.pkg.jetbrains.space/mycompany/p/my-project/filesrepo
+                curl -k "${'$'}backend_server_url\${'$'}TARGET_PATH" --user "${'$'}	backend_server_user:${'$'}backend_server_pw" -T "${'$'}SOURCE_PATH" --ftp-create-dirs
+                curl -i -H "Authorization: Bearer ${'$'}JB_SPACE_CLIENT_TOKEN" -F file=@"${'$'}SOURCE_PATH" ${'$'}REPO_URL/${'$'}TARGET_PATH
             """
         }
     }
